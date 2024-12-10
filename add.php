0e7137217fc58b6c914e5a1c81ad7afeaf3af95b
Samra <your_email@example.com>
2024-12-11 00:38:33 +0300

    form done using POST method

--- add.php
+++ add.php
@@ -1,11 +1,33 @@
+<?php
+// if (isset($_GET['submit'])) {
+//     echo $_GET['email'];
+//     echo $_GET['title'];
+//     echo $_GET['ingredients'];
+// }
+if (isset($_POST['submit'])) {
+    echo $_POST['email'];
+    echo $_POST['title'];
+    echo $_POST['ingredients'];
+}
+?>
+
+
 <!DOCTYPE html>
 <html>
 <?php
 include('templates/header.php'); ?>
 <section class="container grey-text">
     <h4 class="center">Add a Pizza</h4>
-    <form action="" class="white" method="">
-        <label for="">Your Email:</label>
+    <form class="white" action="add.php" method="POST">
+        <label>Your Email:</label>
+        <input type="text" name="email">
+        <label>Pizza Title:</label>
+        <input type="text" name="title">
+        <label>Ingredients (comma separated):</label>
+        <input type="text" name="ingredients">
+        <div class="center">
+            <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
+        </div>
     </form>
 
 </section>
--- templates/header.php
+++ templates/header.php
@@ -10,6 +10,12 @@
         .brand-text {
             color: #cbb09c !important;
         }
+
+        form {
+            max-width: 460px;
+            margin: 20px auto;
+            padding: 20px;
+        }
     </style>
 </head>
 
