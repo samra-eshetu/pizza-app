04b70e9e87ac1e69fc1d529140666ab392a5e1f9
Samra <your_email@example.com>
2024-12-12 01:21:48 +0300

    redered data to the browser

--- index.php
+++ index.php
@@ -6,13 +6,48 @@ $conn = mysqli_connect('localhost', 'samra', '2334', 'samra pizza');
 if (!$conn) {
     echo 'Connection error:' . mysqli_connect_error();
 }
+
+//write query for all pizzas
+
+$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
+
+//make query and get result
+$result = mysqli_query($conn, $sql);
+
+//fetch the resulting rows as an array
+$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+//free result from memory
+mysqli_free_result($result);
+
+//close connection
+mysqli_close($conn);
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <?php
-include('templates/header.php');
-include('templates/footer.php');
+include('templates/header.php'); ?>
+<h4 class="center grey-text">Pizzas!</h4>
+<div class="container">
+    <div class="row">
+        <?php foreach ($pizzas as $pizza) { ?>
+            <div class="col s6 md3">
+                <div class="card z-depth-0">
+                    <div class="card-content center">
+                        <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+                        <div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+                    </div>
+                    <div class="card-action right-align">
+                        <a href="#" class="brand-text">more info</a>
+                    </div>
+                </div>
+            </div>
+            <?php
+        } ?>
+    </div>
+</div>
+<?php include('templates/footer.php'); ?>
 ?>
 
 
