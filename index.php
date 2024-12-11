1d1653bf5e00685f8f013681dd2aadf57ec9185c
Samra <your_email@example.com>
2024-12-12 00:33:51 +0300

    connected to database

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+//connect to database
+$conn = mysqli_connect('localhost', 'samra', '2334', 'samra pizza');
+
+// check the connection
+if (!$conn) {
+    echo 'Connection error:' . mysqli_connect_error();
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <?php
