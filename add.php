f51f1e955d883701095c1ee4dc05fbf93def5b67
Samra <your_email@example.com>
2024-12-11 13:01:04 +0300

    filter and more validation

--- add.php
+++ add.php
@@ -21,15 +21,22 @@ if (isset($_POST['submit'])) {
     if (empty($_POST['title'])) {
         echo 'A title is required <br/>';
     } else {
-        echo htmlspecialchars($_POST['title']);
+        $title = $_POST['title'];
+        if (!preg_match('/^[a-zA-Z\s]+$/', $title)) {
+            echo 'Title must be letters and spaces only.';
+        }
     }
 
     if (empty($_POST['ingredients'])) {
         echo 'At least on ingredient is required <br/>';
     } else {
-        echo htmlspecialchars($_POST['ingredients']);
+        $ingredients = $_POST['title'];
+        if (!preg_match('/^([a-zA-Z\s]+)(,s*[a-zA-Z\s]*)*$/', $ingredients)) {
+            echo 'Ingredients must be a comma separated list';
+        }
     }
 }
+
 ?>
 
 
