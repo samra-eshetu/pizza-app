193885a66bb1bca6ed2d5250841d021096222c62
Samra <your_email@example.com>
2024-12-16 10:26:21 +0300

    deleting a record

--- details.php
+++ details.php
@@ -1,6 +1,18 @@
 <?php
 
 include('config/db_connect.php');
+if (isset($_POST['delete'])) {
+    $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+    $sql = "DELETE FROM pizzas WHERE id= $id_to_delete";
+    if (mysqli_query($conn, $sql)) {
+        //success
+        header('Location: index.php');
+
+    } else {
+        echo 'query error:' . mysqli_error($conn);
+    }
+}
+
 //check GET request id parameter
 
 if (isset($_GET['id'])) {
@@ -9,7 +21,7 @@ if (isset($_GET['id'])) {
     //make sql
     $sql = "SELECT * FROM pizzas WHERE id= $id";
 
-    //get theh query result
+    //get the query result
     $result = mysqli_query($conn, $sql);
 
     //fetch result in array format
@@ -32,6 +44,11 @@ if (isset($_GET['id'])) {
         <p>Created on: <?php echo date('F j, Y, g:i a', strtotime($pizza['created_at'])); ?></p>
         <h5>Ingredients:</h5>
         <p><?php echo htmlspecialchars($pizza['ingredients']); ?></p>
+        <!-- delete form -->
+        <form action="details.php" method="POST">
+            <input type="hidden" name="id_to_delete" value="<?php echo $pizza['id'] ?>">
+            <input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+        </form>
     <?php else: ?>
         <h5 class="red-text">Pizza not found!</h5>
     <?php endif; ?>
